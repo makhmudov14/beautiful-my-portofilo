69c27a20fd78ab498ce7cdea2bbb8a47ddc4bbbc
Umidjon <sokcho69@workershigh.com>
2024-02-16 07:29:26 +0900

    added extra project

--- src/data.jsx
+++ src/data.jsx
@@ -100,4 +100,12 @@ export const projects = [
     title: 'E-comerce App project',
     text: 'Discover the convenience of shopping at your fingertips! With we love comfy, you can explore a vast array of products from the comfort of your home, office, or anywhere you go. Whether you are a fashion enthusiast, tech guru, or home decor lover, weve got something for everyone.',
   },
+  {
+    id: nanoid(),
+    img: 'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8am9ic3RlciUyMHBpY3xlbnwwfHwwfHx8MA%3D%3D',
+    url: 'https://asadbek-jobster.netlify.app/landing',
+    github: 'https://github.com/makhmudov14/jobster',
+    title: 'Job-searching App project',
+    text: 'Welcome to Job Seeker Hub, your go-to platform for navigating the exciting landscape of career opportunities. Whether you are a seasoned professional looking to elevate your career or a recent graduate embarking on your professional journey, Job Seeker Hub is here to make your job search seamless and rewarding',
+  },
 ];
